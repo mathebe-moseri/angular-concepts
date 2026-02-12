45d710d9836186dc763377cb427b688c19b8d211
mathebe <mathebemoseri@gmail.com>
2026-02-12 23:17:52 +0200

    feat: explain and practically-implement two-way data binding

--- src/app/components/data-binding/two-way-binding/two-way-binding.component.ts
+++ src/app/components/data-binding/two-way-binding/two-way-binding.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-two-way-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './two-way-binding.component.html',
   styleUrl: './two-way-binding.component.css'
 })
 export class TwoWayBindingComponent {
+
+
+  question1: string = "What is Two-Way Data Binding in Angular?";
+  answer1: string = "Two-way binding in Angular allows data to flow in both directions: from the component to the view and from the view back to the component. This means when the component value changes, the view updates automatically, and when the user types or changes input in the view, the component property updates immediately. Two-way binding uses the [(ngModel)] syntax on form elements like input fields.";
+
+  explanation: string = "In this example, we use [(ngModel)] on an input field to bind it to the component property 'name'. Any changes in the input field automatically update the component property, and any changes in the component property automatically update the input field. This demonstrates how two-way binding keeps the component and view in sync.";
+
+  question: string = "Why is two-way data binding useful?";
+  answer: string = "Two-way binding is useful because it allows the component and the view to stay synchronized automatically, reducing the need for manual event handling or property updates.";
+
+  name: string = "";
+
+  constructor() {
+    console.log(this.name);
+  }
+
+  onInput(event: any) {
+  console.log(event.target.value);
+  }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { StringInterpolationComponent } from '../components/data-binding/string-interpolation/string-interpolation.component';
 import { PropertyBindingComponent } from '../components/data-binding/property-binding/property-binding.component';
 import { EventBindingComponent } from '../components/data-binding/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from '../components/data-binding/two-way-binding/two-way-binding.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent],
+  imports: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayBindingComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })

352544f84abcfa2f37357c1ea49b6f16bc9cc3eb
mathebe <mathebemoseri@gmail.com>
2026-02-18 15:14:42 +0200

    feat: life-cycle-hooks

---
+++ src/app/components/lifecycle-hooks/lifecycle-hooks.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle-hooks',
+  standalone: true,
+  imports: [],
+  templateUrl: './lifecycle-hooks.component.html',
+  styleUrl: './lifecycle-hooks.component.css'
+})
+export class LifecycleHooksComponent {
+
+  constructor() {
+    console.log("Inside Constructor");
+  }
+
+  ngOnInit(): void {
+    console.log("Inside ngOnInit");
+  }
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,11 +7,13 @@ import { DirectivesComponent } from '../components/directives/directives.compone
 import { PipesComponent } from '../components/decorators/pipes/pipes.component';
 import { LoginComponent } from '../components/login/login.component';
 import { MenuComponent } from '../components/menu/menu.component';
+import { LifecycleHooksComponent } from '../components/lifecycle-hooks/lifecycle-hooks.component';
+import { ParentComponent } from '../components/parent/parent.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayBindingComponent, DirectivesComponent, PipesComponent, LoginComponent, MenuComponent],
+  imports: [StringInterpolationComponent, PropertyBindingComponent, EventBindingComponent, TwoWayBindingComponent, DirectivesComponent, PipesComponent, LoginComponent, MenuComponent, LifecycleHooksComponent, ParentComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
